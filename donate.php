81ae665861167e4208a2a14613129c8106ad6d3d
Brad Sawicki <git@bradsawicki.com>
2014-06-03 17:03:34 -0500

    Donate Page / Store Tee img / Btn color update

---
+++ donate.php
@@ -0,0 +1,59 @@
+<?php $body_class = 'page-donate'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/header.php'; ?>
+	
+  <section class="hero">
+    <div class="wrapper">
+    
+      <div class="hero-content">
+        <h1 class="title">Donate</h1>
+        <p>Thank you for choosing to support Curing Kids Cancer. Even a small amount of money can make a big difference in pediatric cancer; it can make the difference between a child surviving or not.</p>
+      </div>
+
+    </div>
+  </section>
+    
+  <section class="donate-info-blocks">
+    <div class="wrapper">
+
+      <div class="grid">
+      
+        <div class="col-1-3">
+          <h2>General Donation</h2>
+          <p>General donations to support Curing Kids Cancer's mission of finding cures for childhood cancers in our lifetime.</p>
+          <p><a href="" class="btn">Donate Now</a></p>
+        </div>
+
+        <div class="col-1-3">
+          <h2>In Memory or Honor Donations</h2>
+          <p>Donations in honor or memory of a special person or event.</p>
+          <p><a href="" class="btn">Donate Now</a></p>
+        </div>
+
+        <div class="col-1-3">
+          <h2>Support a Fundraiser</h2>
+          <p>Join Craig Kimbrel and other fundraisers, in Curing Kids Cancer.</p>
+          <p><a href="" class="btn">Donate Now</a></p>
+        </div>
+
+      </div>
+      
+    </div>
+  </section>
+  
+  <section class="visit-store">
+    <div class="wrapper">
+            
+        <div class="visit-store-content">
+          <h2 class="title-section">Get a Gift For Your Coach!</h2>
+          <p>Looking for a touching way to say thank you to your coach, teacher or other special person in your life? Now, you can purchase honorary certificates and other Curing Kids Cancer gear as a way to do just that. Check out the latest CKC gear and place your order today!</p>
+          <p><a href="" class="btn">Visit Our Store</a></p>
+        </div>
+
+        <div class="visit-store-image">
+          <img src="/assets/images/pic-store-tee-01.png">
+        </div>
+      
+    </div>
+  </section>
+  
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'; ?>
