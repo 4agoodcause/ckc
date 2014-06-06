45c5224ba5c7fa1c14726fc6abf5488e1197b3e7
Brad Sawicki <git@bradsawicki.com>
2014-06-06 11:54:18 -0500

    Content/Sidebar mobile padding fix

--- views/footer.php
+++ views/footer.php
@@ -1,32 +1,36 @@
   <section class="footer-top">
     <div class="wrapper">
+      
+      <div class="grid">
 
-      <div class="col-1-3 follow-us">
-        <h3 class="title-block">Connect with Us</h3>
-        <ul class="social-icons">
-          <li><a href="" class="icon-social-lg-twitter"></a></li>
-          <li><a href="" class="icon-social-lg-facebook"></a></li>
-          <li><a href="" class="icon-social-lg-youtube"></a></li>
-          <li><a href="" class="icon-social-lg-flickr"></a></li>
-        </ul>
-      </div>
-      
-      <div class="col-1-3 post-list-small">
-        <h3 class="title-block">News &amp; Events</h3>
-        <article class="post">
-          <p class="post-date">June 15, 2014</p>
-          <h4 class="post-title"><a href="">Lorem ipsum dolor sit amet, consect adipiscing elit, verbose encontro</a></h4>
-        </article>
-        <article class="post">
-          <p class="post-date">June 15, 2014</p>
-          <h4 class="post-title"><a href="">Lorem ipsum dolor sit amet, consect adipiscing elit, verbose encontro</a></h4>
-        </article>
-      </div>
-      
-      <div class="col-1-3">
-        <h3 class="title-block">Join the Battle</h3>
-        <p>Join our campaign to cure pediatric cancer. Learn 5 things you can do today to help.</p>
-        <p><a href="" class="btn trans-border">Join the Battle</a></p>
+        <div class="col-1-3 follow-us">
+          <h3 class="title-block">Connect with Us</h3>
+          <ul class="social-icons">
+            <li><a href="" class="icon-social-lg-twitter"></a></li>
+            <li><a href="" class="icon-social-lg-facebook"></a></li>
+            <li><a href="" class="icon-social-lg-youtube"></a></li>
+            <li><a href="" class="icon-social-lg-flickr"></a></li>
+          </ul>
+        </div>
+        
+        <div class="col-1-3 post-list-small">
+          <h3 class="title-block">News &amp; Events</h3>
+          <article class="post">
+            <p class="post-date">June 15, 2014</p>
+            <h4 class="post-title"><a href="">Lorem ipsum dolor sit amet, consect adipiscing elit, verbose encontro</a></h4>
+          </article>
+          <article class="post">
+            <p class="post-date">June 15, 2014</p>
+            <h4 class="post-title"><a href="">Lorem ipsum dolor sit amet, consect adipiscing elit, verbose encontro</a></h4>
+          </article>
+        </div>
+        
+        <div class="col-1-3">
+          <h3 class="title-block">Join the Battle</h3>
+          <p>Join our campaign to cure pediatric cancer. Learn 5 things you can do today to help.</p>
+          <p><a href="" class="btn trans-border">Join the Battle</a></p>
+        </div>
+        
       </div>
 
     </div>
