0f7879d1b8907a7caeaeb90257fc4375df9b9cb0
Brad Sawicki <git@bradsawicki.com>
2014-06-06 11:07:13 -0500

    Hero Imgs / Post page / Body typography

---
+++ post.php
@@ -0,0 +1,78 @@
+<?php $body_class = 'page-post'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/header.php'; ?>
+	
+  <section class="hero">
+    <div class="wrapper">
+      <h1 class="title">News</h1>
+    </div>
+  </section>
+    
+  <section class="main">
+    <div class="wrapper">
+            
+      <section class="content">
+        
+        <article class="post">
+          <h2 class="post-title">News Title Here Lorem Ipsum</h2>
+          <p class="post-date">June 15, 2013</p>
+          <div class="body">
+            <p>Ut consectetur fermentum nisi, sit amet consectetur nisi cursus sed. Curabitur scelerisque accumsan purus, elementum congue purus placerat et. Vivamus mollis tellus eget ipsum tristique, sit amet pretium erat interdum. Donec eu euismod ligula. Curabitur tristique vestibulum nunc. Maecenas pharetra vulputate nisi vel gravida. Vestibulum at sapien accumsan purus sodales facilisis sed eu erat. Etiam luctus ullamcorper magna in rhoncus. Fusce eu risus quis risus ornare vulputate vel nec dui. Sed tincidunt bibendum ipsum mollis accumsan. Suspendisse mollis at magna non dapibus. Curabitur vel tempor erat. Integer vitae commodo dui.</p>
+            <h3>Integer nec rhoncus enim</h3>
+            <p>Phasellus aliquam velit vitae odio auctor gravida. Donec in orci vel sapien suscipit accumsan. In viverra, orci non accumsan aliquet, ante velit iaculis augue, et iaculis magna quam sit amet est. Nunc condimentum, eros quis interdum mattis, nisi turpis imperdiet nibh, in venenatis lectus risus a sem. Praesent in tincidunt est. Cras sagittis varius dapibus. Curabitur diam enim, consequat sit amet volutpat sed, consectetur sit amet nisl. Sed placerat libero in molestie euismod. Suspendisse id varius quam, quis pharetra neque. In ac nisi eu felis bibendum convallis eu dignissim magna. Maecenas luctus orci non nibh luctus, et ultrices sapien accumsan.</p>
+            <figure>
+              <img src="/assets/images/fpo-01.jpg" alt="">
+              <figcaption>This is the caption of this image</figcaption>
+            </figure>
+            <h4>Lorem ipsum dolor sit amet</h4>
+            <p>In hac habitasse platea dictumst. Consectetur adipiscing elit. Vestibulum eget justo laoreet, malesuada tellus vel, gravida quam. Pellentesque interdum consectetur lorem, vel pellentesque est scelerisque vel. Cras faucibus vestibulum diam ut euismod. Ut bibendum sem eget purus hendrerit, eget venenatis dolor cursus. Etiam pharetra faucibus velit vitae ullamcorper. Duis pretium metus eget condimentum pulvinar.</p>
+            <ul>
+              <li>Unordered List Item 1</li>
+              <li>Unordered List Item 2</li>
+              <li>Unordered List Item 3</li>
+              <li>Unordered List Item 4</li>
+            </ul>
+          </div>
+        </article>
+      
+      </section>
+
+      <aside class="sidebar-left">
+      
+        <nav class="block side-menu">
+          <ul>
+            <li><a href="" class="active">News</a></li>
+            <li><a href="">Press Releases</a></li>
+            <li><a href="">Events</a></li>
+            <li><a href="">Event Photos</a></li>
+            <li><a href="">Newsletter Archive</a></li>
+          </ul>
+        </nav>
+        
+        <div class="block block-share">
+          <h3 class="title-block">Share This Page</h3>
+          <ul class="social-icons">
+            <li><a href="" class="icon-social-sm-twitter"></a></li>
+            <li><a href="" class="icon-social-sm-facebook"></a></li>
+            <li><a href="" class="icon-social-sm-youtube"></a></li>
+            <li><a href="" class="icon-social-sm-flickr"></a></li>
+          </ul>
+        </div>
+
+        <div class="block block-media">
+          <h3 class="title-block">Media Inquiries</h3>
+          <p class="media-name">Clay Owen</p>
+          <p class="media-email"><a href="mailto:clay@curingkidscancer.org">clay@curingkidscancer.org</a></p>
+        </div>
+
+        <div class="block block-join-battle">
+          <h3 class="title-block">Join the Battle</h3>
+          <p>Join our campaign to cure pediatric cancer. Learn 5 things you can do today to help.</p>
+          <p><a href="" class="btn btn-small">Join the Battle</a></p>
+        </div>
+        
+      </aside>
+            
+    </div>
+  </section>
+  
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'; ?>
